db2ef41277c5aa9511f8589719a8dc995d6db3f2
chinyan <chinyan@lifelinelab.io>
2024-02-19 13:57:25 +0800

    add uuid validation for model

--- src/model/base_model.py
+++ src/model/base_model.py
@@ -1,13 +1,13 @@
-from pydantic import BaseModel as PydanticBaseModel, constr
+from pydantic import BaseModel as PydanticBaseModel, constr, Field
 from datetime import datetime
 from ..database.mongodb.collection import Collections
 
 uuid_pattern = r"[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89aAbB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}"
 
 class BaseModel(PydanticBaseModel):
-    _key:constr(pattern=uuid_pattern) 
+    key:constr(pattern=uuid_pattern) = Field(alias="_key")
 
-    created_by:constr(pattern=f"{Collections.PROFILES}/{uuid_pattern}")
+    created_by:constr(pattern=f"{Collections.PROFILES}/{uuid_pattern}") # type: ignore
     created_at:datetime
     updated_by:constr(pattern=f"{Collections.PROFILES}/{uuid_pattern}")
     updated_at:datetime
