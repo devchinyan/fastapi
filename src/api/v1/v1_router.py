ff2fb8def012c9445cb97c70a220a5fecf1c3197
chinyan <chinyan@lifelinelab.io>
2024-03-03 18:20:35 +0800

    include profile groups routers

--- src/api/v1/v1_router.py
+++ src/api/v1/v1_router.py
@@ -1,11 +1,17 @@
 from ..route_model import RouteMatrices
 from typing import List
 from .auth.auth_router import auth_route_matrices
+from .profile.profile_router import profile_route_matrices
 
 v1_route_matrices:List[RouteMatrices] = [
     RouteMatrices(
         group = "auth",
         tag="auth",
         route_matrices=auth_route_matrices
+    ),
+    RouteMatrices(
+        group = "profile",
+        tag="profile",
+        route_matrices=profile_route_matrices
     )
 ]
