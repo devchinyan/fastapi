1b7bf29365ba8e0432aa2d5e99c999875c974d19
chinyan <chinyan@lifelinelab.io>
2024-03-03 18:24:06 +0800

    improve static typing

--- src/api/public/public_controller.py
+++ src/api/public/public_controller.py
@@ -2,18 +2,19 @@
 from .public_response import HealthCheckResponse
 from .public_payload import EchoPayload
 from ..route_model import ControllerResponse
-from ...helper.print.colorlog import ColorLog
+from ...helper.cryptography.jwt import JWT_data
+from ...helper.doc.doc_helper import FetchedData
 from typing import Any, Optional,List,Dict
 from fastapi import Request
 
-async def health_check_handler(req:Request,jwtData:Optional[dict], validated_payload:None, fetched_data:List[Dict], params:Any)->ControllerResponse:
+async def health_check_handler(req:Request,jwtData:Optional[JWT_data], validated_payload:None, fetched_data:FetchedData, params:Any)->ControllerResponse:
     try:
         return ControllerResponse(res=HealthCheckResponse(message = "api up and running"))
     except Exception as error:
 
         return ControllerResponse(err=error,status_code=500)
     
-async def echo_handler(req:Request,jwtData:Optional[dict], validated_payload:EchoPayload, fetched_data:List[Dict], params:Any)->ControllerResponse:
+async def echo_handler(req:Request,jwtData:Optional[JWT_data], validated_payload:EchoPayload, fetched_data:FetchedData, params:Any)->ControllerResponse:
     try:
         return ControllerResponse(res=HealthCheckResponse(message = f"You were sending {validated_payload.message}"))
     except Exception as error:
