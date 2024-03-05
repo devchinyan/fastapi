67702c50b826731d5a8adaf09cc2dabd62ec7cf4
chinyan <chinyan@lifelinelab.io>
2024-03-05 11:32:05 +0800

    fetch env from commandline if env file not exist

--- src/config/config.py
+++ src/config/config.py
@@ -1,5 +1,6 @@
 from dotenv import dotenv_values
 from pydantic_settings import BaseSettings
+from os import environ
 
 class Config(BaseSettings):
     HOST:str
@@ -13,4 +14,12 @@ class Config(BaseSettings):
     JWT_EXPIRE_MINUTES:int
     JWT_REFRESH_MINUTES:int
     
-config = Config(**dotenv_values("env/.env")) 
+def process_env(filepath:str)->Config:
+    env_var = dotenv_values(filepath) 
+    keys = list(Config.__annotations__.keys())
+    for key in keys:
+        env_var[key] = env_var.get(key) if env_var.get(key) is not None else environ.get(key)
+
+    return Config(**env_var) 
+
+config = process_env("env/.env")
